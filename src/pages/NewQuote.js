841e5e2f2c48031d8d4bf36551b97758cc365ce4
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-28 12:45:08 +0100

    implemented imperative navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,15 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
-  const addQuoteHandler = () => {};
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+
+    history.push("/quotes");
+  };
 
   return <QuoteForm addQuoteData={addQuoteHandler} />;
 };

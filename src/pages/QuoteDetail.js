dc646841e09f63f0024d4441ba92ae836a3b628b
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-28 07:18:32 +0100

    Nested the routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
   return (
     <Fragment>
       <h1>Quote Detail</h1>;<p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`} exact>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };

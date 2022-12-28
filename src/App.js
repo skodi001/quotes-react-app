8c9a7ecbba491b8d600992ec7fcd5fc74022069d
Shokhmurod <shokhpjwstk@gmail.com>
2022-12-28 06:24:07 +0100

    prepared the starting project's components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId" exact>
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote" exact>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/components/comments/NewCommentForm.js
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+
+import classes from "./NewCommentForm.module.css";
+
+const NewCommentForm = (props) => {
+  const commentTextRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      <div className={classes.control} onSubmit={submitFormHandler}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button className="btn">Add Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewCommentForm;
